679a25c5398ebbb9c9c267faca55201bbb587c0f
PPetrov <syncredex@gmail.com>
2023-05-25 18:08:33 +0300

    Change: delete unused methods

--- lib/feature/category/repository/category_repository.dart
+++ lib/feature/category/repository/category_repository.dart
@@ -1,19 +1,8 @@
 import '../../../base/repository/base_repository.dart';
 import '../../../constant/db_constant.dart';
 import '../../category/model/category.dart';
-import '../../products/model/product.dart';
 
 class CategoryRepository extends BaseRepository {
-  Future<List<Product>> fetchProducts() => restClient.fetchProducts();
-
-  Future<List<Object?>> saveProducts(List<Product> products) async {
-    return await dbClient.insertAll(products);
-  }
-
-  Future<List<Product>> getProducts() async {
-    return await dbClient.getProducts();
-  }
-
   Future<List<Category>> getCategories() async {
     List<Map<String, dynamic>> categoriesInfo = await dbClient.getCategories();
     return categoriesInfo
